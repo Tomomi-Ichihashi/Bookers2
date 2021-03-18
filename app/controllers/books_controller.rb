8ddd0adc3e8ac6428983b0b385e272061845f67e
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-18 09:44:48 +0000

    [Add]create機能＆show.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,20 +4,27 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
-    @user= current_user
+    @user = current_user
+    @users = User.all
   end
   
   def show
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def edit
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id 
+    if @book.save
+      redirect_to book_path(@book.id)
+    end
   end
+  
+  
   
   def destroy
   end
