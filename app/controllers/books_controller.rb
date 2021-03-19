cc4126420f4f0cd5bc8ddb63e8ff7e95401bb051
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-19 10:24:09 +0000

    [Add]投稿更新 成功＆失敗メッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id 
+    
     if @book.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
@@ -18,6 +19,7 @@ class BooksController < ApplicationController
       @books = Book.all
       render 'index'
     end
+    
   end
   
   
@@ -32,8 +34,15 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    
+    if@book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'edit'
+    end
+    
   end  
   
  
